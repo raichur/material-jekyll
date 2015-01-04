f2b0ecc95ad932baaff2819fce93ee2bd42d6089
Joshua Raichur <hello@joshuaraichur.com>
2015-01-04 22:25:32 +0530

    Menu

---
+++ README.md
@@ -0,0 +1,3 @@
+# Material Jekyll
+
+A simple Jekyll theme inspired by Google's new visual language, Material Design. It's a work in progress (It'll be ready soon). You can see it's half-baked version [here](http://joshuaraichur.com/material-jekyll) if you want to.
--- _assets/js/main.js
+++ _assets/js/main.js
@@ -33,17 +33,78 @@ shortcut.add("Shift+Up", function() {
 
 $(".wrapper h1, .wrapper p, section").velocity("transition.slideDownIn", 400, { stagger: 550 });
 $(".paper-button").delay(100).velocity("transition.fadeIn", 250);
+$('#home').velocity("scroll", 1000)
+.velocity({ opacity: 1 });
+/*
+* Nav
+*/
 
 function openCloseNav() {
   var nav = $(this);
+  var list = $('.list');
+  var back = $('#bg-blur');
+  var body = $('body');
   if (nav.hasClass('opened')) {
     nav.addClass('closed');
     nav.one('transitionend', function() {
       nav.removeClass('opened closed');
+      list.velocity("transition.slideLeftBigOut", 250);
+      back.velocity("transition.fadeOut", 200);
+      body.removeClass('fixed');
     });
   } else {
     nav.addClass('opened');
+    list.velocity("transition.slideLeftBigIn", 200);
+    back.velocity("transition.fadeIn", 200);
+    body.addClass('fixed');
   }
 }
 
 $('nav').click(openCloseNav);
+$('#bg-blur').click(function(event) {
+  if($('nav').is(":visible")) {
+    var nav = $('nav')
+    var list = $('.list');
+    var back = $('#bg-blur');
+    var body = $('body');
+    nav.addClass('closed');
+    nav.one('transitionend', function() {
+      nav.removeClass('opened closed');
+      list.velocity("transition.slideLeftBigOut", 250);
+      back.velocity("transition.fadeOut", 200);
+      body.removeClass('fixed');
+    });
+  }
+});
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+function hasScrolled() {
+  var st = $(this).scrollTop();
+  if(Math.abs(lastScrollTop - st) <= delta)
+    return;
+    if (st > lastScrollTop && st > navbarHeight){
+      $('nav').addClass('nav-hide');
+    } else {
+      if(st + $(window).height() < $(document).height()) {
+        $('nav').removeClass('nav-hide');
+      }
+    }
+
+    lastScrollTop = st;
+  }
